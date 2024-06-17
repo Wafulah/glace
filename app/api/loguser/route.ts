a3d3aa90a811115ba68a7a85b5adc134a19b0ab5
wafulahvictor@gmail.com <wafulahvictor@gmail.com>
2024-06-17 16:59:52 +0300

    route

--- app/api/loguser/route.ts
+++ app/api/loguser/route.ts
@@ -1,25 +1,28 @@
-// pages/api/loguser.ts
-
-import { NextApiRequest, NextApiResponse } from 'next';
+import { redirect } from "next/navigation";
+import { NextApiRequest, NextApiResponse } from "next";
 import { signIn } from "@/auth";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { session_token, user_info } = req.query;
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  const { searchParams } = new URL(req.url as string);
+  const session_token = searchParams.get("session_token");
+  const user_info = searchParams.get("user_info");
 
   if (!session_token || !user_info) {
-    return res.status(400).json({ error: 'Missing session_token or user_info' });
+    return res
+      .status(400)
+      .json({ error: "Missing session_token or user_info" });
   }
 
   try {
-    const user = JSON.parse(decodeURIComponent(user_info as string));
+    const user = JSON.parse(decodeURIComponent(user_info));
 
-    await signIn('credentials', {
+    await signIn("credentials", {
       user,
     });
 
-    res.redirect('/settings');
+    redirect("/settings");
   } catch (error) {
-    console.error('Error logging in user:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error logging in user:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 }
