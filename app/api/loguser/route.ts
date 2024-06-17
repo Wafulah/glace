56753eaf7b2e2f9207464c8b012a703d6766a3d6
wafulahvictor@gmail.com <wafulahvictor@gmail.com>
2024-06-17 17:06:16 +0300

    setting

--- app/api/loguser/route.ts
+++ app/api/loguser/route.ts
@@ -1,16 +1,18 @@
 import { redirect } from "next/navigation";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
+
 import { signIn } from "@/auth";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   const { searchParams } = new URL(req.url as string);
   const session_token = searchParams.get("session_token");
   const user_info = searchParams.get("user_info");
 
   if (!session_token || !user_info) {
-    return res
-      .status(400)
-      .json({ error: "Missing session_token or user_info" });
+    return NextResponse.json(
+      { error: "Missing session_token or user_info" },
+      { status: 400 }
+    );
   }
 
   try {
@@ -23,6 +25,9 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
     redirect("/settings");
   } catch (error) {
     console.error("Error logging in user:", error);
-    res.status(500).json({ error: "Internal server error" });
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
   }
 }
