5477446c233831c92fad863a859d2a9aa5f18b3d
wafulahvictor@gmail.com <wafulahvictor@gmail.com>
2024-06-18 01:56:01 +0300

    nexturl

--- middleware.ts
+++ middleware.ts
@@ -1,59 +1,60 @@
 import NextAuth from "next-auth";
-
+import { NextResponse } from "next/server";
 import authConfig from "@/auth.config";
 import {
-    DEFAULT_LOGIN_REDIRECT,
-    apiAuthPrefix,
-    authRoutes,
-    publicRoutes,
-    
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+  loginApiCall,
 } from "@/routes";
 
 const { auth } = NextAuth(authConfig);
-const allowedOrigins = [
-    "https://sandbox.safaricom.co.ke",
-    "https://sokofiti-admin.vercel.app",
-    "https://api.safaricom.co.ke",
-    process.env.FRONTEND_STORE_URL, // Add your frontend store URL
-];
 
 export default auth((req) => {
-    const { nextUrl } = req;
-    const isLoggedIn = !!req.auth;
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-    
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-    
-    if (isApiAuthRoute) {
-        return;
-    }
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isLoginApiCall = nextUrl.pathname.startsWith(loginApiCall);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    if (isAuthRoute) {
-        if (isLoggedIn) {
-            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-        }
-        return;
-    }
-    
-    if (!isLoggedIn && !isPublicRoute) {
-        let callbackUrl = nextUrl.pathname;
-        if (nextUrl.search) {
-            callbackUrl += nextUrl.search;
-        }
-
-        const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-
-        // return Response.redirect(
-        //     new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
-        // );
-    }
-
+  if (isApiAuthRoute) {
     return;
+  }
+
+  if (isAuthRoute && !isLoginApiCall) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  if (isLoginApiCall) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+    // return Response.redirect(
+    //     new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+    // );
+  }
+
+  return;
 });
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
--- routes.ts
+++ routes.ts
@@ -3,10 +3,7 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = [
-  "/",
-  "/auth/new-verification"
-];
+export const publicRoutes = ["/", "/auth/new-verification"];
 
 /**
  * An array of routes that are used for authentication
@@ -18,8 +15,10 @@ export const authRoutes = [
   "/auth/register",
   "/auth/error",
   "/auth/reset",
-  "/auth/new-password"
+  "/auth/new-password",
 ];
+
+export const loginApiCall = "/api/loguser";
 
 /**
  * The prefix for API authentication routes
