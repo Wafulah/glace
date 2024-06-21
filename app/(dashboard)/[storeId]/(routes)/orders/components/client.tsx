b84e70c36fa2efbbef9fe3e452354ce838576e0a
wafulahvictor@gmail.com <wafulahvictor@gmail.com>
2024-06-21 15:56:50 +0300

    order

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useParams, useRouter } from "next/navigation";
+
 import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
@@ -10,25 +12,26 @@ import { ApiList } from "@/components/ui/api-list";
 import { columns, OrderColumn } from "./columns";
 
 interface OrderClientProps {
-    data: OrderColumn[];
+  data: OrderColumn[];
 }
 
 export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
-    return (
-        <>
-            <Heading
-                title={`Orders (${data.length})`}
-                description="Manage orders for your store"
-            />
-            <Button onClick={() => router.push(`/${params.storeId}/orders/new`)}>
-          <Plus className="mr-2 h-4 w-4" /> Add New
-        </Button>
-            <Separator />
-            <DataTable searchKey="buyer" columns={columns} data={data} />
-            <Heading title="API" description="API Calls for Orders" />
+  const params = useParams();
+  const router = useRouter();
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
+      <Button onClick={() => router.push(`/${params.storeId}/orders/new`)}>
+        <Plus className="mr-2 h-4 w-4" /> Add New
+      </Button>
+      <Separator />
+      <DataTable searchKey="buyer" columns={columns} data={data} />
+      <Heading title="API" description="API Calls for Orders" />
       <Separator />
       <ApiList entityName="orders" entityIdName="orderId" />
-        </>
-    );
+    </>
+  );
 };
-
