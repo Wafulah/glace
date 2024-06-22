49a3c6c65beefd0a6d8be18b2a2350e41ab6019d
wafulahvictor@gmail.com <wafulahvictor@gmail.com>
2024-06-22 13:11:02 +0300

    params

--- app/(dashboard)/[storeId]/(routes)/counties/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/counties/components/client.tsx
@@ -27,7 +27,7 @@ export const CountiesClient: React.FC<CountiesClientProps> = ({ data }) => {
           title={`Counties (${data.length})`}
           description="Manage counties for your store"
         />
-        <Button onClick={() => router.push(`admin/${params.storeId}/new-county`)}>
+        <Button onClick={() => router.push(`/${params.storeId}/counties/new`)}>
           <Plus className="mr-2 h-4 w-4" /> Add New
         </Button>
       </div>
