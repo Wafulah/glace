177bed73386d8d03c07adc34539864fa1345a713
wafulahvictor@gmail.com <wafulahvictor@gmail.com>
2024-06-22 12:23:22 +0300

    categories

--- actions/get-categories.ts
+++ actions/get-categories.ts
@@ -1,31 +1,39 @@
 import { Category } from "@/types";
+import { parseISO } from "date-fns";
 
 const API_URL = process.env.NEXT_PUBLIC_API_ALL_URL;
 
-export const getCategories = async (store_id: string, jwt_token: string): Promise<Category[]> => {
-    if (!store_id || !jwt_token) {
-        throw new Error('Store ID and user jwt token are required');
+export const getCategories = async (
+  store_id: string,
+  jwt_token: string
+): Promise<Category[]> => {
+  if (!store_id || !jwt_token) {
+    throw new Error("Store ID and user jwt token are required");
+  }
+
+  const url = `${API_URL}/${store_id}/categories/`;
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${jwt_token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error fetching categories: ${response.statusText}`);
     }
 
-    const url = `${API_URL}/${store_id}/categories/`;
-
-    try {
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${jwt_token}`,
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error(`Error fetching categories: ${response.statusText}`);
-        }
-
-        const categories: Category[] = await response.json();
-        return categories;
-    } catch (error) {
-        console.error('[GET_CATEGORIES_ERROR]', error);
-        throw error;
-    }
+    const categories: Category[] = await response.json();
+    return categories.map((item: any) => ({
+      ...item,
+      created_at: parseISO(item.created_at),
+      updated_at: parseISO(item.updated_at),
+    }));
+  } catch (error) {
+    console.error("[GET_CATEGORIES_ERROR]", error);
+    throw error;
+  }
 };
