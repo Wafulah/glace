ab384a19c520077a55dc8fb9b4b9a75f38102157
wafulahvictor@gmail.com <wafulahvictor@gmail.com>
2024-06-23 12:27:52 +0300

    client

--- app/(dashboard)/[storeId]/(routes)/orders/[orderId]/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/[orderId]/components/client.tsx
@@ -8,42 +8,34 @@ import { Addresscolumns, columns, OrderCol } from "./columns";
 import { OrderForm } from "./order-form";
 
 interface OrderClientProps {
-    data: OrderCol;
+  data: OrderCol;
 }
 
 export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
-    const newOrderData = [
-        {
-            phone: data.phone,
-            address: data.address,
-            isPaid: data.isPaid,
-            isDelivered: data.isDelivered,
-            created_at: data.created_at,
-        },
-    ];
+  const newOrderData = [
+    {
+      phone: data.phone,
+      address: data.address,
+      isPaid: data.is_paid,
+      isDelivered: data.is_delivered,
+      created_at: data.created_at,
+    },
+  ];
 
-    const dataDelivery = { isDelivered: data.isDelivered,deliveryDate:data.deliveryDate };
+  const dataDelivery = {
+    isDelivered: data.is_delivered,
+    deliveryDate: data.delivery_date,
+  };
 
-    return (
-        <>
-            <Heading
-                title={`Orders (${data.id})`}
-                description="Manage order for your store"
-            />
-            <Separator />
-
-            <DataTable
-                searchKey="product"
-                columns={Addresscolumns}
-                data={newOrderData}
-            />
-            <DataTable
-                searchKey="name"
-                columns={columns}
-                data={data.products}
-            />
-            <OrderForm initialData={dataDelivery} />
-        </>
-    );
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.id})`}
+        description="Manage order for your store"
+      />
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data.products} />
+      <OrderForm initialData={dataDelivery} />
+    </>
+  );
 };
-
