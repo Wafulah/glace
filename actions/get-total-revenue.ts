eaa100ff9099fb84f97d82a8db00515b122c6893
wafulahvictor@gmail.com <wafulahvictor@gmail.com>
2024-06-23 11:43:53 +0300

    get-total-revenue

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -5,7 +5,7 @@ export const getTotalRevenue = async (storeId: string, jwt_token: string) => {
   const paidOrders = await getOrders(storeId, jwt_token);
 
   const totalRevenue = paidOrders.reduce((total, order) => {
-    const orderTotal = order.orderItems.reduce((orderSum, item) => {
+    const orderTotal = order.order_items.reduce((orderSum, item) => {
       return orderSum + Number(item.product.price);
     }, 0);
     return total + orderTotal;
