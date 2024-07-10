285e5e89751fa535eb005f7e07ed5d95e94571af
wafulahvictor@gmail.com <wafulahvictor@gmail.com>
2024-07-10 22:40:22 +0300

    orders

--- app/(dashboard)/[storeId]/(routes)/orders/[orderId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/[orderId]/page.tsx
@@ -55,7 +55,8 @@ const ProductPage = async ({
         }))
       : [],
   };
-
+ console.log("page", order.order_items)
+ console.log("order", order);
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
